4f6cc2163c7efca47b39580846667da7c392d214
hmgmishia <h.itpcmain.kmg@gmail.com>
2020-11-23 07:31:25 +0900

    inspectorで指定できるようにscriptableobjectを追加

---
+++ Assets/Plugins/hmgm/UnityLocalize/Scripts/Scriptable/LocalizeTextScriptableObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace hmgm.UnityLocalize.Scriptable
+{
+    [CreateAssetMenu(menuName = "Localize/Text ScriptableObject")]
+    public class LocalizeTextScriptableObject : ScriptableObject
+    {
+        [SerializeField]
+        private LocalizeTextData data;
+
+        /// <summary>
+        /// ローカライズしたテキストを取得
+        /// </summary>
+        /// <param name="language"></param>
+        /// <returns></returns>
+        public string GetLocalizeText(SystemLanguage language)
+        {
+            return data.GetLocalizeText(language);
+        }
+
+        /// <summary>
+        /// アプリケーションの言語設定に従いテキストを取得
+        /// </summary>
+        /// <returns></returns>
+        public string GetLocalizeText()
+        {
+            return data.GetLocalizeText(Application.systemLanguage);
+        }
+
+        public LocalizeTextScriptableObject(LocalizeTextData data)
+        {
+            this.data = data;
+        }
+    }
+   
+}
